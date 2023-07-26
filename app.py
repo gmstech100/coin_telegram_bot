2f2dded9bfa4c85310451488991900ecff4d6d8b
namng <nam.nn@bidgear.com>
2023-07-27 00:49:43 +0700

    edit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
 from apis import MONGO_DETAILS
 from models import TokenModel
+from process import processing_coin_info
 
 
 app = FastAPI()
@@ -13,7 +14,14 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 db = client.college
 
 @app.post("/", response_description="Add new token", response_model=TokenModel)
-async def create_student(token: TokenModel = Body(...)):
+async def add_token(token_name:str, token_url:str):
+    base_token_address, base_token_pool_id = processing_coin_info(token_url)
+    token_dict = {
+        "name": token_name,
+        "token": base_token_address,
+        "pool_id": base_token_pool_id,
+    }
+    token = TokenModel(**token_dict)
     token = jsonable_encoder(token)
     new_token = await db["coin_bot"].insert_one(token)
     created_token = await db["coin_bot"].find_one({"_id": new_token.inserted_id})
