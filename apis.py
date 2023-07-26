a4463edde4e9c760cc3cab89117b173eb909980e
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-07-26 15:08:45 +0700

    edit

---
+++ apis.py
@@ -0,0 +1,2 @@
+GET_TRADE_HISTORY = 'https://api.coinmarketcap.com/kline/v3/k-line/transactions/1/{}?'
+GET_TOKEN_INFO = 'https://api.coinmarketcap.com/dexer/v3/dexer/pair-list?'
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from apis import GET_TOKEN_INFO, GET_TRADE_HISTORY
+
 import requests
+import time
 
 def get_coin_info(base_token_address):
-    api_url = 'https://api.coinmarketcap.com/dexer/v3/dexer/pair-list?'
     params = {
         'base-address': base_token_address,
         'start':1,
         'limit':10,
         'platform-id':1
     }
-    return requests.get(api_url, params=params).json()
+    return requests.get(GET_TOKEN_INFO, params=params).json()
+
+def get_token_transaction(pool_id):
+    api_url = GET_TRADE_HISTORY.format(pool_id)
+    params = {
+        'reverse-order':False
+    }
+    print(requests.get(GET_TRADE_HISTORY, params=params).json())
+    return requests.get(GET_TRADE_HISTORY, params=params).json()
 
 url = 'https://coinmarketcap.com/dexscan/ethereum/0x7235c4aa48b753e48c3786ec60d3bddef5f4b27a/'
 page = Request(url=url,headers={'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(urlopen(page).read(),'html.parser')
 base_token_address = soup.find('div',{'class':'sc-3514e8c3-0'}).find('a')['href'].split('/')[-1]
-print(base_token_address)
 base_token_pool_id = get_coin_info(base_token_address)['data'][0]['poolId']
-print(base_token_pool_id)
+latest_transaction = get_token_transaction(base_token_pool_id)['data']['transactions'][0]
+print(latest_transaction)
