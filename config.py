4833daa53b7ba0b1386c6834e4840e132143eb90
namng <nam.nn@bidgear.com>
2023-07-29 23:44:23 +0700

    edit

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 GET_TRADE_HISTORY = 'https://api.coinmarketcap.com/kline/v3/k-line/transactions/1/{}?'
 GET_TOKEN_INFO = 'https://api.coinmarketcap.com/dexer/v3/dexer/pair-list?'
+GET_BASE_TOKEN = 'https://api.dexscreener.com/latest/dex/search'
 
 MONGO_DETAILS = "mongodb://localhost:27017"
 
--- process.py
+++ process.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-from config import GET_TOKEN_INFO, GET_TRADE_HISTORY
+from config import GET_TOKEN_INFO, GET_TRADE_HISTORY, GET_BASE_TOKEN
 from database import database
 from loguru import logger
 from models import NETWORK_PLATFORM_ID
 
 import requests
 import time
+import re
 
 def get_coin_info(base_token_address, network):
     params = {
@@ -24,11 +25,15 @@ def get_token_transaction(pool_id):
     }
     return requests.get(api_url, params=params).json()
 
+def get_base_token_address(url):
+    params = {
+        'q':url.split('/')[-1]
+    }
+    return requests.get(GET_BASE_TOKEN, params=params).json()
+
 def processing_coin_info(url, network):
-    try:
-        page = Request(url=url,headers={'User-Agent': 'Mozilla/5.0'})
-        soup = BeautifulSoup(urlopen(page).read(),'html.parser')
-        base_token_address = soup.find('div',{'class':'frteJV'}).find('a')['href'].split('/')[-1]
+    try:        
+        base_token_address = get_base_token_address(url=url)['pairs']['baseToken']['address']
         logger.info(base_token_address)
         base_token_pool_id = get_coin_info(base_token_address, network)['data'][0]['poolId']
         logger.info(base_token_pool_id)
@@ -45,4 +50,7 @@ async def insert_token(token):
 
 async def get_tokens():
     return database['tokens'].find()
+
+
+    
 
