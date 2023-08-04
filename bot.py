b543a08b9aea4bdb70e64af7c4ce4240865e5f1f
Ngo Ngoc nam <namngocngo22@gmail.com>
2023-08-04 13:19:56 +0700

    edit

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from transaction import EthereumTransaction
 from database import database
 from telegram_handler import TelegramBot
 from token_socket import read_socket
+from loguru import logger
 
 telegram_bot = TelegramBot(BOT_TOKEN, CHAT_ID)
 telegram_bot.run()
@@ -86,7 +87,7 @@ async def send_token_list_to_telegram(tokens):
 async def process_token_trade_history(token, count):
     global transaction_count
     trade_api = GET_TRADE_HISTORY.format(token['pool_id'])
-    print('pool_id', token['pool_id'])
+    logger.info('pool_id %s' % token['pool_id'])
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188'
     }
@@ -97,69 +98,61 @@ async def process_token_trade_history(token, count):
             data = await response.json()
             list_trades = data['data']['transactions']
     if last_transaction is None:
-        first_trade = list_trades[0]
-        if first_trade['type'] == 'buy':
-            await database["transactions"].insert_one(
-                {'pool_id': token['pool_id'], 'last_transaction': json.dumps(first_trade)})
-            from_address = EthereumTransaction(INFURA_ID).get_transaction_by_hash(first_trade['txn'])['from']
-            display_from_address = '{}...{}'.format(from_address[:6], from_address[-4:])
-            eth_value = round(usd_to_eth(float(first_trade['totalUsd'])), 2)
-            try:
-                current_market_cap = read_socket(token["network"], token["pair_address"])['pair']['marketCap']
-            except Exception as ex:
-                current_market_cap = 0
-            telegram_bot.send_message(
-                message_text=telegram_message_format.format(format_count(count), token['base_token_name'],
-                                                            token['token_telegram'], token['base_token_name'],
-                                                            token['token_telegram'], token['description'], eth_value,
-                                                            round(float(first_trade['totalUsd']), 2),
-                                                            display_from_address, from_address, first_trade['txn'],
-                                                            '{:,}'.format(current_market_cap), token['chart'],
-                                                            token['trade'], token['snipe'], token['trending']),
-                button_text=token['ads_text'], button_url=token['ads_url'])
-            transaction_count += 1
-        else:
-            print('no new transaction')
+        for trade in list_trades:
+            if trade['type'] == 'buy':
+                await database["transactions"].insert_one(
+                    {'pool_id': token['pool_id'], 'last_transaction': json.dumps(trade)})
+                from_address = EthereumTransaction(INFURA_ID).get_transaction_by_hash(trade['txn'])['from']
+                display_from_address = '{}...{}'.format(from_address[:6], from_address[-4:])
+                eth_value = round(usd_to_eth(float(trade['totalUsd'])), 2)
+                try:
+                    current_market_cap = read_socket(token["network"], token["pair_address"])['pair']['marketCap']
+                except Exception as ex:
+                    current_market_cap = 0
+                telegram_bot.send_message(
+                    message_text=telegram_message_format.format(format_count(count), token['base_token_name'],
+                                                                token['token_telegram'], token['base_token_name'],
+                                                                token['token_telegram'], token['description'],
+                                                                eth_value,
+                                                                round(float(trade['totalUsd']), 2),
+                                                                display_from_address, from_address, trade['txn'],
+                                                                '{:,}'.format(current_market_cap), token['chart'],
+                                                                token['trade'], token['snipe'], token['trending']),
+                    button_text=token['ads_text'], button_url=token['ads_url'])
+                transaction_count += 1
+            else:
+                logger.info('NO NEW TRANSACTION')
     else:
         json_last_transaction = json.loads(last_transaction['last_transaction'])
-        index_last_transaction = 0
         for trade in list_trades:
-            if trade['time'] == json_last_transaction['time']:
-                index_last_transaction = list_trades.index(trade)
-        if index_last_transaction != 0:
-            print('index', index_last_transaction)
-            if index_last_transaction == 1:
-                new_list_trades = list_trades[:index_last_transaction]
-            else:
-                new_list_trades = list_trades[:index_last_transaction - 1]
-            for new_trade in new_list_trades:
-                if new_trade['type'] == 'buy':
-                    last_transaction_update = await database["transactions"].update_one(
-                        {"pool_id": token['pool_id']},
-                        {"$set": {'last_transaction': json.dumps(new_trade)}}
-                    )
-                    if last_transaction_update.modified_count == 1:
-                        print('last transaction updated to db')
-                    from_address = EthereumTransaction(INFURA_ID).get_transaction_by_hash(new_trade['txn'])['from']
-                    display_from_address = '{}...{}'.format(from_address[:6], from_address[-4:])
-                    eth_value = round(usd_to_eth(float(new_trade['totalUsd'])), 2)
-                    try:
-                        current_market_cap = read_socket(token["network"], token["pair_address"])['pair']['marketCap']
-                    except Exception as ex:
-                        current_market_cap = 0
-                    telegram_bot.send_message(
-                        message_text=telegram_message_format.format(format_count(count), token['base_token_name'],
-                                                                    token['token_telegram'], token['base_token_name'],
-                                                                    token['token_telegram'], token['description'],
-                                                                    eth_value, round(float(new_trade['totalUsd']), 2),
-                                                                    display_from_address, from_address,
-                                                                    new_trade['txn'], '{:,}'.format(current_market_cap),
-                                                                    token['chart'], token['trade'], token['snipe'],
-                                                                    token['trending']), button_text=token['ads_text'],
-                        button_url=token['ads_url'])
-                    transaction_count += 1
+            if int(trade['time']) > int(json_last_transaction['time']) and trade['type'] == 'buy':
+
+                last_transaction_update = await database["transactions"].update_one(
+                    {"pool_id": token['pool_id']},
+                    {"$set": {'last_transaction': json.dumps(trade)}}
+                )
+                if last_transaction_update.modified_count == 1:
+                    print('last transaction updated to db')
+                from_address = EthereumTransaction(INFURA_ID).get_transaction_by_hash(trade['txn'])['from']
+                display_from_address = '{}...{}'.format(from_address[:6], from_address[-4:])
+                eth_value = round(usd_to_eth(float(trade['totalUsd'])), 2)
+                try:
+                    current_market_cap = read_socket(token["network"], token["pair_address"])['pair']['marketCap']
+                except Exception as ex:
+                    current_market_cap = 0
+                telegram_bot.send_message(
+                    message_text=telegram_message_format.format(format_count(count), token['base_token_name'],
+                                                                token['token_telegram'], token['base_token_name'],
+                                                                token['token_telegram'], token['description'],
+                                                                eth_value, round(float(trade['totalUsd']), 2),
+                                                                display_from_address, from_address,
+                                                                trade['txn'], '{:,}'.format(current_market_cap),
+                                                                token['chart'], token['trade'], token['snipe'],
+                                                                token['trending']), button_text=token['ads_text'],
+                    button_url=token['ads_url'])
+                transaction_count += 1
         else:
-            print('no new transaction')
+            logger.info('NO NEW TRANSACTION')
 
     # Increment the transaction count
 
